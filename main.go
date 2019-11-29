f50d528dac5c9bb489a82a3f93d2a77689c938a9
Harold Alcala <harold.dev@gmail.com>
2019-11-29 18:27:16 +0800

    Tests

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"runtime"
+	"strings"
 	"time"
 )
 
@@ -21,10 +23,305 @@ func pow(x, n, lim float64) float64 {
 	return lim
 }
 
+func Sqrt(x float64) float64 {
+	for z := float64(1); z*z <= x; z += 1 {
+		if z*z == x {
+			return z
+		}
+	}
+
+	return -1
+}
+
+func show_os() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
+
+func check_day() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
+
+func check_day2() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
+
+func test_defer() {
+	defer fmt.Println("world") // same as process.nextTick() in NodeJS
+
+	fmt.Println("hello")
+}
+
+func test_pointer() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+}
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func test_struc() {
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+}
+
+func test_struc_pointer() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+}
+
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
+func test_array() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
+
+func test_array_slice() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+}
+
+func test_array_reference() {
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
+
+func test_array_slice2() {
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+}
+
+func test_array_slice3() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func test_array_slice4() {
+	a := make([]int, 5)
+	printSlice2("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice2("b", b)
+
+	c := b[:2]
+	printSlice2("c", c)
+
+	d := c[2:5]
+	printSlice2("d", d)
+}
+
+func printSlice2(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
+
+func test_array_slice5() {
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+func test_array_slice6() {
+	var s []int
+	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// We can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+var pow2 = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func test_range() {
+	for i, v := range pow2 {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+func test_range2() {
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
+
+// func Pic(dx, dy int) [][]uint8 {
+// }
+
+// func test_array_slice7() {
+// 	pic.Show(Pic)
+// }
+
 func main() {
 	fmt.Println("Welcome to the playground!")
 
 	fmt.Println("The time is", time.Now(), rand.Intn(10))
 
 	fmt.Println(add(42, rand.Intn(10)))
+
+	show_os()
+
+	check_day()
+
+	check_day2()
+
+	test_defer()
+
+	fmt.Println("Was defered?")
+
+	test_pointer()
+
+	test_struc()
+
+	test_struc_pointer()
+
+	fmt.Println("fmt.Println(v1, p, v2, v3) ⌵ ⌵ ⌵ ⌵ ⌵ ⌵")
+	fmt.Println(v1, p, v2, v3)
+
+	test_array()
+
+	test_array_slice()
+
+	test_array_reference()
+
+	test_array_slice2()
+
+	test_array_slice3()
+
+	test_array_slice4()
+
+	test_array_slice5()
+
+	test_array_slice6()
+
+	test_range()
+
+	test_range2()
+
+	// test_array_slice7()
 }

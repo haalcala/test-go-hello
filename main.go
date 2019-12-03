54ab2f95f1937070b50b86701d0452222a4e2835
Harold Alcala <harold.dev@gmail.com>
2019-12-03 13:17:07 +0800

    Tests

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"io"
 	"math"
 	"math/rand"
+	"os"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
+
+	"golang.org/x/tour/tree"
 )
 
 func add(x int, y int) int {
@@ -452,6 +455,65 @@ func test_type() {
 	fmt.Println("t", t.(string), "ok", ok, t == nil)
 }
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %0.2f", float64(e))
+}
+
+func Sqrt2(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	return 0, nil
+}
+
+func test_error() {
+	fmt.Println("------------------------------------------------------ test_error")
+
+	fmt.Println(Sqrt2(2))
+
+	res, err := Sqrt2(-2)
+
+	fmt.Println("res:", res, "err", err)
+}
+
+func rot13(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return (b-a+13)%(z-a+1) + a
+}
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	for i := 0; i < n; i++ {
+		p[i] = rot13(p[i])
+	}
+	return
+}
+
+func test_error2() {
+	fmt.Println("------------------------------------------------------ test_error2")
+
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+
+	fmt.Println()
+}
+
 func test_map_types() {
 	hosts := map[string]interface{}{
 		"loopback":  []int{127, 0, 0, 1},
@@ -479,6 +541,148 @@ func test_io() {
 		if err == io.EOF {
 			break
 		}
+	}
+}
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func test_goroutine() {
+	fmt.Println("------------------------------------------------------ test_goroutine")
+
+	go say("world")
+	say("hello")
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func test_channel() {
+	fmt.Println("------------------------------------------------------ test_channel")
+
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
+
+func test_switch_default() {
+	fmt.Println("------------------------------------------------------ test_switch_default")
+
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+
+	var i = time.Now()
+
+	for {
+		select {
+		case i = <-tick:
+			fmt.Println("tick. i:", i)
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    . i:", i)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func test_select() {
+	fmt.Println("------------------------------------------------------ test_select")
+
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	fibonacci(c, quit)
+}
+
+func walkImpl(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	walkImpl(t.Left, ch)
+	ch <- t.Value
+	walkImpl(t.Right, ch)
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	walkImpl(t, ch)
+	// Need to close the channel here
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+// NOTE: The implementation leaks goroutines when trees are different.
+// See binarytrees_quit.go for a better solution.
+func Same(t1, t2 *tree.Tree) bool {
+	w1, w2 := make(chan int), make(chan int)
+
+	go Walk(t1, w1)
+	go Walk(t2, w2)
+
+	for {
+		v1, ok1 := <-w1
+		v2, ok2 := <-w2
+		if !ok1 || !ok2 {
+			return ok1 == ok2
+		}
+		if v1 != v2 {
+			return false
+		}
+	}
+}
+
+func test_binary_tree() {
+	fmt.Println("------------------------------------------------------ test_binary_tree")
+
+	fmt.Print("tree.New(1) == tree.New(1): ")
+	if Same(tree.New(1), tree.New(1)) {
+		fmt.Println("PASSED")
+	} else {
+		fmt.Println("FAILED")
+	}
+
+	fmt.Print("tree.New(1) != tree.New(2): ")
+	if !Same(tree.New(1), tree.New(2)) {
+		fmt.Println("PASSED")
+	} else {
+		fmt.Println("FAILED")
 	}
 }
 
@@ -554,7 +758,21 @@ func main() {
 
 	test_map_types()
 
+	test_error()
+
+	test_error2()
+
 	test_io()
+
+	test_goroutine()
+
+	test_channel()
+
+	test_switch_default()
+
+	test_select()
+
+	test_binary_tree()
 
 	vcm := &VCMessenger{}
 
